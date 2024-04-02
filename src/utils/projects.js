3c0524fee0c7381fa2334c9b020427af18d9d7e5
thomastepi <tepexminem2013@gmail.com>
2024-04-02 01:52:52 -0400

    refactor projects array

---
+++ src/utils/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+  {
+    title: "Sawyer-Camp Farmers CIG",
+    description: `Developed and launched a website for Sawyer Camp Farmers Common Initiative Group, empowering local farmers in Cameroon by showcasing their products and sustainable farming practices. Implemented user-friendly design and seamless navigation to enhance user experience and engagement.`,
+    getImageSrc: () => require("../assets/images/sawyer-camp.png"),
+    link: "https://sawyer-camp-farmers.onrender.com/",
+    github: "https://github.com/thomastepi/sawyer-camp",
+  },
+  {
+    title: "AI Resume Builder",
+    description:
+      "An innovative AI-powered resume builder, provides a secure platform for users to log in, update their profiles, and create professional resumes. With the option to choose from existing templates or generate a personalized one using advanced AI algorithms, ResumeCraft ensures that resumes are tailored to each user's unique profile. ",
+    getImageSrc: () => require("../assets/images/resume-builder.png"),
+    link: "https://resume-craft.onrender.com/",
+    github: "https://github.com/thomastepi/resume-craft.git",
+  },
+  {
+    title: "BookMart",
+    description:
+      "A secure and user-friendly book management application that allows users to log in securely and perform essential CRUD (Create, Read, Update, Delete) operations on their book inventory. With an intuitive interface, users can easily add new books, update existing entries, and remove items they no longer need.",
+    getImageSrc: () => require("../assets/images/bookmart.png"),
+    link: "https://bookmart-trw5.onrender.com",
+    github: "https://github.com/thomastepi/book-mart-react-app.git",
+  },
+  {
+    title: "Annette's Beauty Salon",
+    description:
+      "This application showcases a curated gallery of exquisite hairstyles and services offered at Annette's Beauty Salon. As users explore the diverse range of braids and weaves, they can effortlessly schedule appointments, making the experience seamless and tailored to their preferences. ",
+    getImageSrc: () => require("../assets/images/beauty-salon.png"),
+    link: "https://github.com/thomastepi/beauty-salon-react-app.git",
+    github: "https://github.com/thomastepi/beauty-salon-react-app.git",
+  },
+];
+
+export default projects;
