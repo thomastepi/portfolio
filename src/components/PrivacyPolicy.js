9779665a76e1b7c3c44424badbca0b19b256120b
thomastepi <tepexminem2013@gmail.com>
2025-01-27 02:49:19 -0500

    update privacy policy

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -9,16 +9,28 @@ import {
   Button,
   Text,
   Link,
+  Heading,
+  Divider,
+  useColorMode,
 } from "@chakra-ui/react";
-
 const PrivacyPolicy = ({ t, isOpen, onClose }) => {
+  const { colorMode } = useColorMode();
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered size="xl">
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        size="xl"
+        scrollBehavior="inside"
+      >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{t("policy.title")}</ModalHeader>
           <ModalBody>
+            <Heading size="sm" mb={4}>
+              {t("policy.name")}
+            </Heading>
             <Text mb={4}>{t("policy.text1")}</Text>
             <Text mb={4}>{t("policy.text11")}</Text>
             <Text mb={4}>{t("policy.text2")}</Text>
@@ -32,6 +44,23 @@ const PrivacyPolicy = ({ t, isOpen, onClose }) => {
                 {t("policy.linkText")}
               </Link>
               .
+            </Text>
+            <Divider my={4} />
+            <Heading size="sm" mb={4}>
+              {t("localStorage.title")}
+            </Heading>
+            <Text mb={4}>{t("localStorage.intro")}</Text>
+            <Text mb={4}>- {t("localStorage.preferences.colorMode")}</Text>
+            <Text mb={4}>- {t("localStorage.preferences.language")}</Text>
+            <Text mb={4}>{t("localStorage.disclaimer")} </Text>
+            <Divider my={4} />
+
+            <Text
+              mt={4}
+              fontSize="sm"
+              color={colorMode === "light" ? "gray.600" : "gray.400"}
+            >
+              {t("localStorage.lastUpdated")}{" "}
             </Text>
           </ModalBody>
           <ModalFooter>
